8ec277b01dc68c66e03b6c723ec22aacb44489c3
yukoga <yukoga@gmail.com>
2013-03-10 06:32:35 +0900

    develop enhcms_util.js

--- uisample/enhcms_util.js
+++ uisample/enhcms_util.js
@@ -1,108 +1,4 @@
-var _eastPrefObj = [
-  { prefcode: "01", prefname: "北海道" },
-  { prefcode: "02", prefname: "青森県" },
-  { prefcode: "03", prefname: "岩手県" },
-  { prefcode: "04", prefname: "宮城県" },
-  { prefcode: "05", prefname: "秋田県" },
-  { prefcode: "06", prefname: "山形県" },
-  { prefcode: "07", prefname: "福島県" },
-  { prefcode: "08", prefname: "茨城県" },
-  { prefcode: "09", prefname: "栃木県" },
-  { prefcode: "10", prefname: "群馬県" },
-  { prefcode: "11", prefname: "埼玉県" },
-  { prefcode: "12", prefname: "千葉県" },
-  { prefcode: "13", prefname: "東京都" },
-  { prefcode: "14", prefname: "神奈川県" },
-  { prefcode: "15", prefname: "新潟県" },
-  { prefcode: "19", prefname: "山梨県" },
-  { prefcode: "20", prefname: "長野県" },
-  { prefcode: "21", prefname: "岐阜県" },
-  { prefcode: "22", prefname: "静岡県" },
-  { prefcode: "23", prefname: "愛知県" },
-  { prefcode: "24", prefname: "三重県" }
-];
-
-var _westPrefObj = [
-  { prefcode: "16", prefname: "富山県" },
-  { prefcode: "17", prefname: "石川県" },
-  { prefcode: "18", prefname: "福井県" },
-  { prefcode: "25", prefname: "滋賀県" },
-  { prefcode: "26", prefname: "京都県" },
-  { prefcode: "27", prefname: "大阪府" },
-  { prefcode: "28", prefname: "兵庫県" },
-  { prefcode: "29", prefname: "奈良県" },
-  { prefcode: "30", prefname: "和歌山県" },
-  { prefcode: "31", prefname: "鳥取県" },
-  { prefcode: "32", prefname: "島根県" },
-  { prefcode: "33", prefname: "岡山県" },
-  { prefcode: "34", prefname: "広島県" },
-  { prefcode: "35", prefname: "山口県" },
-  { prefcode: "36", prefname: "徳島県" },
-  { prefcode: "37", prefname: "香川県" },
-  { prefcode: "38", prefname: "愛媛県" },
-  { prefcode: "39", prefname: "高知県" },
-  { prefcode: "40", prefname: "福岡県" },
-  { prefcode: "41", prefname: "佐賀県" },
-  { prefcode: "42", prefname: "長崎県" },
-  { prefcode: "43", prefname: "熊本県" },
-  { prefcode: "44", prefname: "大分県" },
-  { prefcode: "45", prefname: "宮崎県" },
-  { prefcode: "46", prefname: "鹿児島県" },
-  { prefcode: "47", prefname: "沖縄県" }
-];
-
-var _prefObj = [
-  { prefcode: "01", prefname: "北海道" },
-  { prefcode: "02", prefname: "青森県" },
-  { prefcode: "03", prefname: "岩手県" },
-  { prefcode: "04", prefname: "宮城県" },
-  { prefcode: "05", prefname: "秋田県" },
-  { prefcode: "06", prefname: "山形県" },
-  { prefcode: "07", prefname: "福島県" },
-  { prefcode: "08", prefname: "茨城県" },
-  { prefcode: "09", prefname: "栃木県" },
-  { prefcode: "10", prefname: "群馬県" },
-  { prefcode: "11", prefname: "埼玉県" },
-  { prefcode: "12", prefname: "千葉県" },
-  { prefcode: "13", prefname: "東京都" },
-  { prefcode: "14", prefname: "神奈川県" },
-  { prefcode: "15", prefname: "新潟県" },
-  { prefcode: "16", prefname: "富山県" },
-  { prefcode: "17", prefname: "石川県" },
-  { prefcode: "18", prefname: "福井県" },
-  { prefcode: "19", prefname: "山梨県" },
-  { prefcode: "20", prefname: "長野県" },
-  { prefcode: "21", prefname: "岐阜県" },
-  { prefcode: "22", prefname: "静岡県" },
-  { prefcode: "23", prefname: "愛知県" },
-  { prefcode: "24", prefname: "三重県" },
-  { prefcode: "25", prefname: "滋賀県" },
-  { prefcode: "26", prefname: "京都県" },
-  { prefcode: "27", prefname: "大阪府" },
-  { prefcode: "28", prefname: "兵庫県" },
-  { prefcode: "29", prefname: "奈良県" },
-  { prefcode: "30", prefname: "和歌山県" },
-  { prefcode: "31", prefname: "鳥取県" },
-  { prefcode: "32", prefname: "島根県" },
-  { prefcode: "33", prefname: "岡山県" },
-  { prefcode: "34", prefname: "広島県" },
-  { prefcode: "35", prefname: "山口県" },
-  { prefcode: "36", prefname: "徳島県" },
-  { prefcode: "37", prefname: "香川県" },
-  { prefcode: "38", prefname: "愛媛県" },
-  { prefcode: "39", prefname: "高知県" },
-  { prefcode: "40", prefname: "福岡県" },
-  { prefcode: "41", prefname: "佐賀県" },
-  { prefcode: "42", prefname: "長崎県" },
-  { prefcode: "43", prefname: "熊本県" },
-  { prefcode: "44", prefname: "大分県" },
-  { prefcode: "45", prefname: "宮崎県" },
-  { prefcode: "46", prefname: "鹿児島県" },
-  { prefcode: "47", prefname: "沖縄県" }
-];
-
-
-var _prefTable = {
+var allPrefTable = {
   "01": "北海道",
   "02": "青森県",
   "03": "岩手県",
@@ -152,151 +48,197 @@ var _prefTable = {
   "47": "沖縄県"
 };
 
+var eastPrefTable = { 
+  "01": "北海道",
+  "02": "青森県",
+  "03": "岩手県",
+  "04": "宮城県",
+  "05": "秋田県",
+  "06": "山形県",
+  "07": "福島県",
+  "08": "茨城県",
+  "09": "栃木県",
+  "10": "群馬県",
+  "11": "埼玉県",
+  "12": "千葉県",
+  "13": "東京都",
+  "14": "神奈川県",
+  "15": "新潟県",
+  "19": "山梨県",
+  "20": "長野県",
+  "21": "岐阜県",
+  "22": "静岡県",
+  "23": "愛知県",
+  "24": "三重県"
+};
 
-/*
-var EnhCmsObject = function() {
-  if (arguments.length > 1) {
-    throw new TypeError('too many arguments. EnhCmsObject can have only 1 argument.'); 
-  } else if (arguments.length == 1) {
-    var _self = this, args = arguments[0];
-    Object.keys(args).forEach(function(k) { _self[k] = args[k]; });
-  }
+var westPrefTable = { 
+  "16": "富山県",
+  "17": "石川県",
+  "18": "福井県",
+  "25": "滋賀県",
+  "26": "京都県",
+  "27": "大阪府",
+  "28": "兵庫県",
+  "29": "奈良県",
+  "30": "和歌山県",
+  "31": "鳥取県",
+  "32": "島根県",
+  "33": "岡山県",
+  "34": "広島県",
+  "35": "山口県",
+  "36": "徳島県",
+  "37": "香川県",
+  "38": "愛媛県",
+  "39": "高知県",
+  "40": "福岡県",
+  "41": "佐賀県",
+  "42": "長崎県",
+  "43": "熊本県",
+  "44": "大分県",
+  "45": "宮崎県",
+  "46": "鹿児島県",
+  "47": "沖縄県"
+};
+
+var ecmsMap = function() {
+  this.o = new Object();
+  this.k = new Array();
 }
-
-EnhCmsObject.prototype = {
-  keys: function() {
-    return Object.keys(this);
+ecmsMap.prototype = {
+  set : function(key, object) {
+    if (!this.o[key]) { this.k[this.k.length] = key; }
+    this.o[key] = object;
   },
-  values: function() {
-    var _a=[], _self=this;
-    Object.keys(_self).forEach(function(k) {
-      _a.push(_self[k]);
-    });
-    return _a;
-  },
-  equals: function(obj) {
-    var _result=true, _self=this;
-    Object.keys(obj).forEach(function(k) {
-      _result = _result && (_self[k] === obj[k]);
-    });
-    return _result;
-  }
-}
-$.extend(EnhCmsObject.prototype, Object.prototype);
-*/
-
-var EnhPrefObject = function(prefcode, prefname) {
-  this.prefcode = prefcode;
-  this.prefname = prefname;
-}
-EnhPrefObject.prototype = {
-  getPrefCode: function() { return this.prefcode; },
-  getPrefName: function() { return this.prefname; },
-  setPrefCode: function(prefcode) { this.prefcode = prefcode; },
-  setPrefName: function(prefname) { this.prefname = prefname; }
-}
-
-var EnhPrefObjectList = function() {
-  var _self = this;
-  var _list = new Array();
-  if (arguments.length == 1) {
-    var args = arguments[0];
-    if (args instanceof Array) {
-      Object.keys(args).forEach(function(k) {
-        if (!args[k].hasOwnProperty('prefcode') || !args[k].hasOwnProperty('prefname')) {
-          throw new TypeError('You need to set array which has a element type of {prefcode: xx, prefname: yyyy} as an argument.');
-        }
-        var _obj = new EnhPrefObject(args[k].prefcode, args[k].prefname);
-        _list.push(_obj);
-      });
-    } else if (args instanceof Object 
-        && args.hasOwnProperty('prefcode') 
-        && args.hasOwnProperty('prefname')) {
-      var _obj = new EnhPrefObject(args.prefcode, args.prefname);
-      _list.push(_obj);
-    } else {
-      throw new TypeError('Type of arguments seems wrong.');
+  get : function(key) { return this.o[key]; },
+  remove : function(key) { this.o[key] = undefined; },
+  getLength : function() { return this.k.length; },
+  each : function(callback, thisargs) {
+    var _this;
+    if ( this === null ) throw new TypeError('this is null or not defined.');
+    if ( {}.toString.call(callback) !== "[object Function]" ) throw new TypeError(callback + ' is not a function.');
+    if ( thisargs ) _this = thisargs;
+    for (var i=0 ; i<this.getLength() ; i++) {
+      var _key = this.k[i], _obj = this.o[_key];
+      if (_obj) {
+        var _result = callback.call(_this, _key, _obj);
+        if (_result === false) { break; }
+      }
     }
-  } else if (arguments.length > 1) {
-    Object.keys(args).forEach(function(k) {
-      if (!args[k].hasOwnProperty('prefcode') || !args[k].hasOwnProperty('prefname')) {
-        throw new TypeError('You need to set some objects which has a element type of {prefcode: xx, prefname: yyyy} as arguments.');
-      }
-      var _obj = new EnhPrefObject(args[k].prefcode, args[k].prefname);
-      _list.push(_obj);
-    });
-  }
-  this.length = _list.length;
-  return _list;
-}
-
-EnhPrefObjectList.prototype = {
-  getPrefName: function(prefcode) {
-    var _self = this, _prefname = null;
-    Object.keys(_self).forEach(function(k) {
-      if (_self[k].prefcode === prefcode) {
-        _prefname = _self[k].prefname;
-        break;
-      }
-    });
-    return _prefname;
   },
-  addPrefObject: function(obj) {
-    
+  filter : function(callback, thisargs) {
+    var _m = new ecmsMap(), _this;
+    if ( this === null ) throw new TypeError('this is null or not defined.');
+    if ( {}.toString.call(callback) !== "[object Function]" ) throw new TypeError(callback + ' is not a function.');
+    if ( thisargs ) _this = thisargs;
+    for (var i=0; i<this.getLength() ; i++) {
+      var _key = this.k[i], _obj = this.o[_key];
+      if (callback.call(_this, _key, _obj) === true) {
+        _m.set(_key, _obj);        
+      }
+    }
+    return _m;
   },
 }
 
 
-var EnhCmsUtil = function(){}
-
-EnhCmsUtil.prototype = {
+var ecmsUtil = function(){}
+ecmsUtil.prototype = {
   insertDoms: function(target, elem, attr) {
     var _self = this;
     var _elm = document.createElement(elem);
     _elm = this.setAttr(_elm, attr);
     $(target).append(_elm);
   },
-  setAttr: function(elem, attr) {
+  insertDomsForAll: function(target, prefmap) {
     var _self = this;
-    if (!(attr instanceof Array) && !(attr instanceof Object)) throw new TypeError('setAttr should have arguments as an Array or Object.');
-    if (attr instanceof Array) {
-      $.each(attr, function(k, v) {
-        $(elem).attr(v);  
-      });
-    } else {
-      $(elem).attr(attr);
-    }
-    return elem;
-  },
-  insertDomsForAll: function(target, obj) {
+    var _elm = document.createElement('div');
+    this.appendLabelForPref(_elm, 'all', '全国');
+    this.appendUIForAll(_elm, 'all');
+    prefmap.each(function(k, v) {
+      this.appendHiddenForPref(_elm, k);
+    }, _self);
+    $(target).append(_elm);
     console.log("insertDomsForAll");
   },
-  insertDomsForEast: function(target, obj) {
+  insertDomsForEast: function(target, prefmap) {
+    var _self = this;
+    var _elm = document.createElement('div');
+    this.appendLabelForPref(_elm, 'east', '東日本');
+    this.appendUIForAll(_elm, 'east');
+    prefmap.each(function(k, v) {
+      this.appendHiddenForPref(_elm, k);
+    }, _self);
+    $(target).append(_elm);
     console.log("insertDomsForEast");
   },
-  insertDomsForWest: function(target, obj) {
+  insertDomsForWest: function(target, prefmap) {
+    var _self = this;
+    var _elm = document.createElement('div');
+    this.appendLabelForPref(_elm, 'west', '西日本');
+    this.appendUIForAll(_elm, 'west');
+    prefmap.each(function(k, v) {
+      this.appendHiddenForPref(_elm, k);
+    }, _self);
+    $(target).append(_elm);
     console.log("insertDomsForWest");
   },
-  insertDomsForPref: function(target, obj) {
+  insertDomsForPref: function(target, prefmap) {
+    var _self = this;
+    prefmap.each(function(k, v) {
+      var _elm = document.createElement('div');
+      this.appendLabelForPref(_elm, k, v);
+      this.appendUIForPref(_elm, k);
+      this.appendHiddenForPref(_elm, k);
+      $(target).append(_elm);
+    }, _self);
     console.log("insertDomsForPref");
   },
-  appendLabelForPref: function(element, prefcode) {
-    var _prefname = ().prefname;
+  appendLabelForPref: function(element, prefcode, prefname) {
     $(element).attr('class', 'preflabel').attr('id', 'pref_' + prefcode);
-    $(element).append(v.prefname);
+    $(element).append(prefname);
     console.log("appendLabelForPref");
   },
-  appendUIForPref: function(prefcode) {
+  appendUIForPref: function(element, prefcode) {
+    $(element).append('<span class="prefui"><a href="javascript:void(0);" id="addDetailedArea_' 
+        + prefcode + '">詳細地域を入力する</a>&nbsp;|&nbsp;<a href="javascript:void(0);" id="delPrefData_' + prefcode + '">削除する</a></span><br />');
+    $(element).children().children('#delPrefData_' + prefcode).click(function() {
+      $(this).parent().parent().remove();
+    });
+    $(element).children().children('#addDetailedArea_' + prefcode).click(function() {
+      var _elm = document.createElement('textarea');
+      $(_elm).attr({name: 'cityarea_' + prefcode, rows: '10', cols: '100'});
+      $(this).parent().parent().append(_elm);
+    });
     console.log("appendUIForPref");
   },
-  appendHiddenForPref: function(prefcode) {
+  appendUIForAll: function(element, prefcode) {
+    $(element).append('<span class="prefui"><a href="javascript:void(0);" id="delPrefData_' + prefcode + '">削除する</a></span><br />');
+    $(element).children().children('#delPrefData_' + prefcode).click(function() {
+      $(this).parent().parent().remove();
+    });
+    console.log("appendUIForPref");
+  },
+  appendHiddenForPref: function(element, prefcode) {
+    $(element).append('<input type="hidden" name="prefcode[]" value="' + prefcode + '" />');
     console.log("appendHiddenForPref");
   },
 }
 
 
 $(function() {
-  var _util = new EnhCmsUtil();
+  var _util = new ecmsUtil();
+  var _east = new ecmsMap(), _west = new ecmsMap(), _all = new ecmsMap();
+  Object.keys(eastPrefTable).forEach(function(v) {
+    _east.set(v, this[v]);
+  }, eastPrefTable);
+  Object.keys(westPrefTable).forEach(function(v) {
+    _west.set(v, this[v]);
+  }, westPrefTable);
+  Object.keys(allPrefTable).forEach(function(v) {
+    _all.set(v, this[v]);
+  }, allPrefTable);
+  
   $('#prefBoxModal').dialog({
     autoOpen: false,
    	height: 380,
@@ -313,35 +255,19 @@ $(function() {
           || 
           (($.inArray('98', _prefCode) > -1) && ($.inArray('97', _prefCode) > -1))
           ) {
-          _util.insertDomsForAll('#prefData', _prefObj);
+          _util.insertDomsForAll('#prefdata', _all);
 		      console.log("「全国」と「東日本」「西日本」あるいは他の都道府県を選択した場合、すべての都道府県に対してデータが登録されます。");
 		  	} else if ($.inArray("98", _prefCode) > -1) {
-          _util.insertDomsForWest('#prefData', _westPrefObj);
+          _util.insertDomsForWest('#prefdata', _west);
 		      console.log("「西日本」として、西日本のすべての都道府県にデータが登録されます。");
 		  	} else if ($.inArray("97", _prefCode) > -1) {
-          _util.insertDomsForEast('#prefData', _eastPrefObj);
+          _util.insertDomsForEast('#prefdata', _east);
 		      console.log("「東日本」として、東日本のすべての都道府県にデータが登録されます。");
 		  	} else {
-          var _regarr = $(_prefObj).filter(function(k) {
-            return ($.inArray(this.prefcode, _prefCode) > -1);
+          var _insertList = _all.filter(function(k, v) {
+            return ($.inArray(k, _prefCode) > -1); 
           });
-          _util.insertDomsForPref('#prefData', _regarr);
-          $.each(_regarr, function(k, v) {
-            var _elm = document.createElement('div');
-            $(_elm).attr('class', 'preflabel').attr('id', 'pref_' + v.prefcode);
-            $(_elm).append(v.prefname);
-            $(_elm).append('<span class="prefui"><a href="javascript:void(0);" id="addDetailedArea_' + v.prefcode + '">詳細地域を入力する</a>&nbsp;|&nbsp;<a href="javascript:void(0);" id="delPrefData_' + v.prefcode + '">削除する</span><br />');
-            $(_elm).append('<input type="hidden" name="prefcode[]" value="' + v.prefcode + '" />');
-            $(_elm).children().children('#delPrefData_' + v.prefcode).click(function() {
-              $(this).parent().parent().remove();
-            });
-            $(_elm).children().children('#addDetailedArea_' + v.prefcode).click(function() {
-              var _elm = document.createElement('textarea');
-              $(_elm).attr({name: 'underPref_' + v.prefcode, rows: '10', cols: '100'});
-              $(this).parent().parent().append(_elm);
-            });
-            $('#prefdata').append(_elm);
-          });
+          _util.insertDomsForPref('#prefdata', _insertList);
           console.log("都道府県のデータを追加します");
         }
 			 	$(this).dialog('close');		  	  	
